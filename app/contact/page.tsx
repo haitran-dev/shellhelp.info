7fca99212a6c7f0a515e9c19fc6ab8d0f2d4169d
haitrandev <haitranthe.dev@gmail.com>
2023-05-22 21:31:30 +0700

    feat: cool icon

--- app/contact/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Contact() {
-	return <main className='flex min-h-screen flex-col'>Contact page</main>;
-}
--- app/projects/spell-book/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function SpellBook() {
-	return <div className='flex min-h-screen flex-col'>SpellBook</div>;
-}
