f145ba4e4d65efb8490494444c904e476be78174
haitran <haitranthe.dev@gmail.com>
2023-06-29 13:28:38 +0700

    Add description and heading

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,27 @@
-import { Roboto } from 'next/font/google';
-import ThemeModeProvider from 'theme-provider';
-import './globals.css';
+import { Roboto } from "next/font/google";
+import ThemeModeProvider from "theme-provider";
+import "./globals.css";
 
 const roboto = Roboto({
-	weight: ['100', '300', '400', '500', '700', '900'],
-	subsets: ['latin'],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ["latin"],
 });
 
 export const metadata = {
-	title: 'Shell Help',
-	description: 'Explain shell commands with help of Fig autocompletion',
+  title: "Shell Help",
+  description: "Explain shell commands for developers",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang='en' suppressHydrationWarning>
-			<body className={roboto.className}>
-				<ThemeModeProvider>{children}</ThemeModeProvider>
-			</body>
-		</html>
-	);
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={roboto.className}>
+        <ThemeModeProvider>{children}</ThemeModeProvider>
+      </body>
+    </html>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,35 @@
-import ShellSVG from 'static/icons/shell.js';
-import { Icon } from 'components/ui/icons';
-import Terminal from 'components/Terminal';
-import { Tooltip } from 'react-tooltip';
+import ShellSVG from "static/icons/shell.js";
+import { Icon } from "components/ui/icons";
+import Terminal from "components/Terminal";
+import { Tooltip } from "react-tooltip";
 
 export default function App() {
-	return (
-		<main className='h-screen bg-gradient-logo p-2 sm:p-4 overflow-auto'>
-			<div className='flex flex-col mx-auto items-center mt-[50px] sm:mt-[100px] mb-[30px] gap-10'>
-				<Logo />
-				<Terminal />
-			</div>
-			<Tooltip id='tooltip-icon' />
-		</main>
-	);
+  return (
+    <main className="h-screen bg-gradient-logo p-2 sm:p-4 overflow-auto">
+      <div className="flex flex-col mx-auto items-center mt-[50px] sm:mt-[100px] mb-[30px] gap-10">
+        <Logo />
+        <Terminal />
+      </div>
+      <Tooltip id="tooltip-icon" />
+    </main>
+  );
 }
 
 function Logo() {
-	return (
-		<div className='flex gap-4 select-none items-center backdrop-blur-md duration-300 text-slate-300 dark:text-gray-900'>
-			<Icon classNames='!w-[48px] sm:!w-[72px]'>
-				<ShellSVG />
-			</Icon>
-			<span className='text-[32px] sm:text-[60px] font-black'>Help!</span>
-		</div>
-	);
+  return (
+    <>
+      <div className="flex gap-4 select-none items-center backdrop-blur-md duration-300 text-slate-300 dark:text-gray-900">
+        <Icon classNames="!w-[48px] sm:!w-[72px]">
+          <ShellSVG />
+        </Icon>
+        <span className="text-[32px] sm:text-[60px] font-black">Help!</span>
+      </div>
+      <h1 className="text-5xl font-semibold text-white dark:text-black duration-300">
+        Master shell commands with ShellHelp!
+      </h1>
+      <h2 className="text-xl text-white dark:text-black duration-300">
+        Perfect for students and developers. Start learning today!
+      </h2>
+    </>
+  );
 }
